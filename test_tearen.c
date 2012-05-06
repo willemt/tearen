af53b018fa77efbf12560d5ec73f04fe5a46c8b3
Willemt <himself@willemthiart.com>
2012-05-06 19:53:34 +1200

    added files

---
+++ test_tearen.c
@@ -0,0 +1,17 @@
+
+#include <stdbool.h>
+#include <assert.h>
+#include <setjmp.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "CuTest.h"
+
+//#include "tearen.h"
+
+void TestTEAREN_Init(
+    CuTest * tc
+)
+{
+    CuAssertTrue(tc, 0);
+}
